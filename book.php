e9a01c57ce5e84fa311f5dae17e6f650f920b443
Sébastien Lucas <sebastien@slucas.fr>
2012-05-30 15:04:23 +0200

    Should be safer #2

--- book.php
+++ book.php
@@ -123,7 +123,7 @@ class Book extends Base {
         if (!is_null ($se)) {
             $addition = $addition . "<strong>" . localize("content.series") . "</strong>" . str_format (localize ("content.series.data"), $this->seriesIndex, $se->name) . "<br />\n";
         }
-        if (preg_match ("/<\/(div|p)>/", $this->comment))
+        if (preg_match ("/<\/(div|p|a)>/", $this->comment))
         {
             return $addition . str_replace ("<br>", "<br />", $this->comment);
         }
