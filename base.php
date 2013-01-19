05cf561683091c2515df12a12b86396bc1932a8b
Sébastien Lucas <sebastien@slucas.fr>
2013-01-19 07:09:12 +0100

    Release preparation

--- base.php
+++ base.php
@@ -6,7 +6,7 @@
  * @author     Sébastien Lucas <sebastien@slucas.fr>
  */
 
-define ("VERSION", "0.3.0");
+define ("VERSION", "0.3.1");
 date_default_timezone_set($config['default_timezone']);
  
 function getURLParam ($name, $default = NULL) {
