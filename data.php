8cfddc0d110feb787b73045b5e69d991a7d6fcf5
Sébastien Lucas <sebastien@slucas.fr>
2013-01-22 09:05:32 +0100

    Fixed detection of absolute path. Add a Windows mapped drive detection. Reported by Kevnancy

--- data.php
+++ data.php
@@ -101,7 +101,9 @@ class Data extends Base {
             $textData = "&data=" . $idData;
         }
         
-        if (preg_match ('/^\//', $config['calibre_directory']) || ($type == "epub" && $config['cops_update_epub-metadata']))
+        if (preg_match ('/^\//', $config['calibre_directory']) || // Linux /
+            preg_match ('/^\w\:/', $config['calibre_directory']) || // Windows X:
+            ($type == "epub" && $config['cops_update_epub-metadata']))
         {
             if ($type != "jpg") $textData .= "&type=" . $type;
             return new Link ("fetch.php?id=$book->id" . $textData, $mime, $rel, $title);
