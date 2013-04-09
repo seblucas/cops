893855a81d87959f510e7e2701259c4a73649605
Sébastien Lucas <sebastien@slucas.fr>
2013-04-09 21:29:43 +0200

    Hopefully fix the book summary problem with Wamp. re #45

--- base.php
+++ base.php
@@ -28,19 +28,6 @@ function xml2xhtml($xml) {
     '), $xml);
 }
 
-function is_utf8($string) {
-return preg_match('%^(?:  
-[\x09\x0A\x0D\x20-\x7E] # ASCII  
-| [\xC2-\xDF][\x80-\xBF] # non-overlong 2-byte  
-| \xE0[\xA0-\xBF][\x80-\xBF] # excluding overlongs  
-| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2} # straight 3-byte  
-| \xED[\x80-\x9F][\x80-\xBF] # excluding surrogates  
-| \xF0[\x90-\xBF][\x80-\xBF]{2} # planes 1-3  
-| [\xF1-\xF3][\x80-\xBF]{3} # planes 4-15  
-| \xF4[\x80-\x8F][\x80-\xBF]{2} # plane 16  
-)*$%xs', $string);
-}
-
 function display_xml_error($error)
 {
     $return .= str_repeat('-', $error->column) . "^\n";
@@ -81,7 +68,6 @@ function are_libxml_errors_ok ()
 function html2xhtml ($html) {
     $doc = new DOMDocument();
     libxml_use_internal_errors(true);
-    if (is_utf8($html)) {
         $doc->loadHTML('<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></head><body>' . 
                             $html  . '</body></html>'); // Load the HTML
         $output = $doc->saveXML($doc->documentElement); // Transform to an Ansi xml stream
@@ -89,16 +75,6 @@ function html2xhtml ($html) {
         if (preg_match ('#<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></meta></head><body>(.*)</body></html>#ms', $output, $matches)) {
             $output = $matches [1]; // Remove <html><body>
         }
-    } else {
-        $doc->loadHTML($html); // Load the HTML
-        $output = $doc->saveXML($doc->documentElement); // Transform to an Ansi xml stream
-        $output = xml2xhtml($output);
-        $output = xml2xhtml($output);
-        if (preg_match ('#<html><body>(.*)</body></html>#ms', $output, $matches)) {
-            $output = $matches [1]; // Remove <html><body>
-        }
-    }
-    
     /* 
     // In case of error with summary, use it to debug
     $errors = libxml_get_errors();
@@ -110,7 +86,7 @@ function html2xhtml ($html) {
     
     if (!are_libxml_errors_ok ()) $output = "HTML code not valid.";
     
-    
+    libxml_use_internal_errors(false);
     return $output;
 }
 
