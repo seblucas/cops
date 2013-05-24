df03815ec6564d55fa4aef113c441d42ac38fe22
Sébastien Lucas <sebastien@slucas.fr>
2013-05-24 11:07:37 +0200

    remove ugly debug code

--- base.php
+++ base.php
@@ -376,7 +376,7 @@ class Page
             $i = 0;
             foreach ($config['calibre_directory'] as $key => $value) {
                 $nBooks = Book::getBookCount ($i);
-                array_push ($this->entryArray, new Entry ($key . " " . $i, "{$i}:cops:catalog", 
+                array_push ($this->entryArray, new Entry ($key, "{$i}:cops:catalog", 
                                         str_format (localize ("bookword", $nBooks), $nBooks), "text", 
                                         array ( new LinkNavigation ("?" . DB . "={$i}"))));
                 $i++;
