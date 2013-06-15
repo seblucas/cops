1daf2ad0d2ed2eb56b11a185abac06851a2eebd5
Sébastien Lucas <sebastien@slucas.fr>
2013-06-15 08:27:33 +0200

    Move most of javascript in util.js. Enabled paging with arrow keys. Refactor a little. re #73

--- util.js
+++ util.js
@@ -1,3 +1,5 @@
+var template, result;
+
 function htmlEscape(str) {
     return String(str)
             .replace(/&/g, '&amp;')
@@ -6,3 +8,43 @@ function htmlEscape(str) {
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;');
 }
+
+function navigateTo (url) {
+    jsonurl = url.replace ("index", "getJSON");
+    $.getJSON(jsonurl, function(data) {
+        history.pushState(data, "", url);
+        updatePage (data);
+    });
+}
+
+function updatePage (data) {
+    result = template (data);
+    document.title = data.title;
+    $(".container").html (result);
+    
+    ajaxifyLinks ();
+}
+
+function ajaxifyLinks () {
+    if (history.pushState) {
+        $("a[href^='index']").click (function (event) {
+            event.preventDefault(); 
+
+            var url = $(this).attr('href');
+            navigateTo (url);
+        });
+    }
+}
+
+window.onpopstate = function(event) {
+    updatePage (event.state);
+};
+
+ $(document).keydown(function(e){
+    if (e.keyCode == 37 && $("#prevLink").length > 0) {
+        navigateTo ($("#prevLink").attr('href'));
+    }
+    if (e.keyCode == 39  && $("#nextLink").length > 0) {
+        navigateTo ($("#nextLink").attr('href'));
+    }
+});
