10ed3f794428b0f95db3ba6262bf5dadfa5708a9
Sébastien Lucas <sebastien@slucas.fr>
2013-06-16 22:01:13 +0200

    Almost everything is in javascript now ... index.php could be an HTML page. re #73

--- index.php
+++ index.php
@@ -22,28 +22,12 @@
         exit ();
     }
     
-    $withToolbar = false;
-    if (!isset($_COOKIE['toolbar'])) $withToolbar = true;
-    
     header ("Content-Type:application/xhtml+xml;charset=utf-8");
     $page = getURLParam ("page", Base::PAGE_INDEX);
     $query = getURLParam ("query");
     $qid = getURLParam ("id");
     $n = getURLParam ("n", "1");
     $database = GetUrlParam (DB);
-    
-/* Test to see if pages are opened on an Eink screen 
- * test Kindle, Kobo Touch and Sony PRS-T1 Ereader. 
- * HTTP_USER_AGENT = "Mozilla/5.0 (Linux; U; en-us; EBRD1101; EXT) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
- */
-
-    if (preg_match("/(Kobo|Kindle\/3.0|EBRD1101)/", $_SERVER['HTTP_USER_AGENT'])) {
-        $isEink = 1;
-    } else {
-        $isEink = 0;
-    }
-
-
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -52,10 +36,8 @@
     <title>COPS</title>
     <script type="text/javascript" src="<?php echo getUrlWithVersion("js/jquery-1.9.1.min.js") ?>"></script>
     <script type="text/javascript" src="<?php echo getUrlWithVersion("js/jquery.cookies.js") ?>"></script>
-<?php if (getCurrentOption ('use_fancyapps') == 1) { ?>
     <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/fancybox/jquery.fancybox.pack.js") ?>"></script>
     <link rel="stylesheet" type="text/css" href="<?php echo getUrlWithVersion("resources/fancybox/jquery.fancybox.css") ?>" media="screen" />
-<?php } ?>
     <script type="text/javascript" src="<?php echo getUrlWithVersion("js/jquery.sortElements.js") ?>"></script>
     <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/doT/doT.min.js") ?>"></script>
     <script type="text/javascript" src="<?php echo getUrlWithVersion("util.js") ?>"></script>
@@ -68,23 +50,6 @@
     
         $(document).ready(function() {
             // Handler for .ready() called.
-            
-            $("#sort").click(function(){
-                $('.books').sortElements(function(a, b){
-                    var test = 1;
-                    if ($("#sortorder").val() == "desc")
-                    {
-                        test = -1;
-                    }
-                    return $(a).find ("." + $("#sortchoice").val()).text() > $(b).find ("." + $("#sortchoice").val()).text() ? test : -test;
-                });
-            });
-            
-<?php if (getCurrentOption ('use_fancyapps') == 1) { ?>
-            $(".fancycover").fancybox(fancyBoxObject (null, 'image'));
-            
-            $(".fancyabout").fancybox(fancyBoxObject ('COPS <?php echo VERSION ?>', 'ajax'));
-<?php } ?>
             
             $.when($.get('templates/default/header.html'),
                    $.get('templates/default/footer.html'),
