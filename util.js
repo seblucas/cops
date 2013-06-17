aa376d63b26e42728df309cb7cefc4a9f337d910
Sébastien Lucas <sebastien@slucas.fr>
2013-06-17 22:30:09 +0200

    simplify again. re #73

--- util.js
+++ util.js
@@ -16,8 +16,8 @@ function fancyBoxObject (title, type) {
 
 function strformat () {
     var s = arguments[0];
-    for (var i = 0; i < arguments.length - 1; i++) {       
-        var reg = new RegExp("\\{" + i + "\\}", "gm");             
+    for (var i = 0; i < arguments.length - 1; i++) {
+        var reg = new RegExp("\\{" + i + "\\}", "gm");
         s = s.replace(reg, arguments[i + 1]);
     }
     return s;
