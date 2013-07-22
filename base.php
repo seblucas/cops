3ed5685f129e4be1264912a20ac94332542e767a
Sébastien Lucas <sebastien@slucas.fr>
2013-07-22 20:58:17 +0200

    Next version will be 0.6.0. Update README and Changelog

--- base.php
+++ base.php
@@ -6,7 +6,7 @@
  * @author     Sébastien Lucas <sebastien@slucas.fr>
  */
 
-define ("VERSION", "0.5.1");
+define ("VERSION", "0.6.0");
 define ("DB", "db");
 date_default_timezone_set($config['default_timezone']);
  
