46d993d168359d448904e093d4be17968bc5a76f
Didier Corbière <didier.corbiere@opale-concept.com>
2013-09-17 05:48:45 +0100

    Add base path

--- config.php
+++ config.php
@@ -7,5 +7,5 @@
  */
 
     require_once 'config_default.php';
-    if (file_exists('config_local.php'))
+    if (file_exists(dirname(__FILE__). '/config_local.php'))
         require_once 'config_local.php';
