c2633cd87dbd37598355d8c380d37f7a91ba61f5
Sébastien Lucas <sebastien@slucas.fr>
2013-10-12 07:13:41 +0200

    Rename the class name. re #96

--- test/bookTest.php
+++ test/bookTest.php
@@ -3,7 +3,7 @@
 require_once (dirname(__FILE__) . "/config_test.php");
 require_once (dirname(__FILE__) . "/../book.php");
 
-class StackTest extends PHPUnit_Framework_TestCase
+class BookTest extends PHPUnit_Framework_TestCase
 {   
     public function testGetBookCount ()
     {
