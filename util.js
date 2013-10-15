deac045135e7f12940bca65bffc73a3850609ccd
Sébastien Lucas <sebastien@slucas.fr>
2013-10-15 19:07:12 +0200

    Ad the new autocomplete search. re #34

--- util.js
+++ util.js
@@ -256,6 +256,33 @@ function updatePage (data) {
     } else {
         $("#sortForm").hide ();
     }
+    
+    //$('input[name=query]').typeahead('destroy');
+    $('input[name=query]').typeahead([
+    {
+        name: 'search',
+        allowDuplicates: true,
+        minLength : 3,
+        valueKey: 'title',
+        limit: 24,
+        template: templateSuggestion,
+        remote: {
+            url: 'getJSON.php?search=1&db=%DB&query=%QUERY',
+            replace: function (url, query) {
+                return url.replace('%QUERY', query).replace('%DB', currentData.databaseId);
+            }
+        }
+    }
+    ]);
+    
+    $('input[name=query]').bind('typeahead:selected', function(obj, datum) {        
+        //alert(JSON.stringify(obj)); // object
+        // outputs, e.g., {"type":"typeahead:selected","timeStamp":1371822938628,"jQuery19105037956037711017":true,"isTrigger":true,"namespace":"","namespace_re":null,"target":{"jQuery19105037956037711017":46},"delegateTarget":{"jQuery19105037956037711017":46},"currentTarget":
+        //alert(JSON.stringify(datum)); // contains datum value, tokens and custom fields
+        // outputs, e.g., {"redirect_url":"http://localhost/test/topic/test_topic","image_url":"http://localhost/test/upload/images/t_FWnYhhqd.jpg","description":"A test description","value":"A test value","tokens":["A","test","value"]}
+        // in this case I created custom fields called 'redirect_url', 'image_url', 'description'       
+        navigateTo (datum.navlink);
+    });
 }
 
 function navigateTo (url) {
