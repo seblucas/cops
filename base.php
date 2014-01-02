c829a3fe74a1048884e00a053652d9cb78e01f3a
Sébastien Lucas <sebastien@slucas.fr>
2014-01-02 12:23:37 +0100

    Prepare next version

--- base.php
+++ base.php
@@ -6,7 +6,7 @@
  * @author     Sébastien Lucas <sebastien@slucas.fr>
  */
 
-define ("VERSION", "0.9.0");
+define ("VERSION", "0.9.1beta");
 define ("DB", "db");
 date_default_timezone_set($config['default_timezone']);
 
