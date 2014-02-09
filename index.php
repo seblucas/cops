04e22728fad9e5b6c170ade7cc995dab0eb697d0
Sébastien Lucas <sebastien@slucas.fr>
2014-02-09 15:24:54 +0100

    Load the doctype / HTML version / head from a separate file. re #79

--- index.php
+++ index.php
@@ -43,52 +43,20 @@
     }
 
     header ("Content-Type:text/html;charset=utf-8");
-?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-    <link rel="apple-touch-icon" href="./icons/icon57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="./icons/icon72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="./icons/icon114.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="./icons/icon144.png" />
-    <meta name="msapplication-TileColor" content="#123456"/>
-    <meta name="msapplication-TileImage" content="./icons/icon144.png"/>
 
-    <title>COPS</title>
-
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/jQuery/jquery-1.10.2.min.js") ?>"></script>
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/jquery-cookie/jquery.cookies.js") ?>"></script>
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/jquery-sortelements/jquery.sortElements.js") ?>"></script>
-<?php if (!useServerSideRendering ()) { ?>
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/Magnific-Popup/jquery.magnific-popup.min.js") ?>"></script>
-    <link rel="stylesheet" type="text/css" href="<?php echo getUrlWithVersion("resources/Magnific-Popup/magnific-popup.css") ?>" media="screen" />
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/doT/doT.min.js") ?>"></script>
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/lru/lru.js") ?>"></script>
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("resources/typeahead/typeahead.js") ?>"></script>
-<?php } ?>
-    <script type="text/javascript" src="<?php echo getUrlWithVersion("util.js") ?>"></script>
-    <link rel="related" href="<?php echo $config['cops_full_url'] ?>feed.php" type="application/atom+xml;profile=opds-catalog" title="<?php echo $config['cops_title_default']; ?>" />
-    <link rel="icon" type="image/vnd.microsoft.icon" href="<?php echo $config['cops_icon']; ?>" />
-    <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Open+Sans:400,300italic,800,300,400italic,600,600italic,700,700italic,800italic&subset=latin,cyrillic' />
-    <link rel="stylesheet" type="text/css" href="<?php echo getUrlWithVersion("resources/normalize/normalize.css") ?>" />
-    <link rel="stylesheet" type="text/css" href="<?php echo getUrlWithVersion("styles/font-awesome.css") ?>" media="screen" />
-    <link rel="stylesheet" type="text/css" href="<?php echo getUrlWithVersion(getCurrentCss ()) ?>" media="screen" />
-<?php if (!useServerSideRendering ()) { ?>
-    <script type="text/javascript">
-
-        $(document).ready(function() {
-            initiateAjax ("<?php echo "getJSON.php?" . addURLParameter (getQueryString (), "complete", 1); ?>",
-                          "<?php echo getCurrentTemplate (); ?>");
-        });
-
-
-
-    </script>
-<?php } ?>
-</head>
-<body>
+    $data = array("title"                 => $config['cops_title_default'],
+                  "version"               => VERSION,
+                  "opds_url"              => $config['cops_full_url'] . "feed.php",
+                  "template"              => getCurrentTemplate (),
+                  "server_side_rendering" => useServerSideRendering (),
+                  "current_css"           => getCurrentCss (),
+                  "favico"                => $config['cops_icon'],
+                  "getjson_url"           => "getJSON.php?" . addURLParameter (getQueryString (), "complete", 1));
+    $headcontent = file_get_contents('templates/' . getCurrentTemplate () . '/file.html');
+    $template = new doT ();
+    $dot = $template->template ($headcontent, NULL);
+    echo ($dot ($data));
+?><body>
 <?php
 if (useServerSideRendering ()) {
     // Get the data
