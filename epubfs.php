2648b9310b2168e59ad9a9be1a897594822bf8d9
Sébastien Lucas <sebastien@slucas.fr>
2014-03-13 18:11:33 +0100

    Fix headers. re #52

--- epubfs.php
+++ epubfs.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * COPS (Calibre OPDS PHP Server) class file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Sébastien Lucas <sebastien@slucas.fr>
+ */
 
 require_once ("config.php");
 require_once ("base.php");
