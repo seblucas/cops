3aebffc2173d8c2707691c1f203f49819f26e4c5
Sébastien Lucas <sebastien@slucas.fr>
2014-07-29 22:15:45 +0200

    With Kindle browser reduce a little the text-size, seems to help. re #165

--- index.php
+++ index.php
@@ -48,11 +48,15 @@
     $data = array("title"                 => $config['cops_title_default'],
                   "version"               => VERSION,
                   "opds_url"              => $config['cops_full_url'] . "feed.php",
+                  "customHeader"          => "",
                   "template"              => getCurrentTemplate (),
                   "server_side_rendering" => useServerSideRendering (),
                   "current_css"           => getCurrentCss (),
                   "favico"                => $config['cops_icon'],
                   "getjson_url"           => "getJSON.php?" . addURLParameter (getQueryString (), "complete", 1));
+    if (preg_match("/Kindle/", $_SERVER['HTTP_USER_AGENT'])) {
+        $data ["customHeader"] = '<style media="screen" type="text/css"> html { font-size: 75%; -webkit-text-size-adjust: 75%; -ms-text-size-adjust: 75%; }</style>';
+    }
     $headcontent = file_get_contents('templates/' . getCurrentTemplate () . '/file.html');
     $template = new doT ();
     $dot = $template->template ($headcontent, NULL);
