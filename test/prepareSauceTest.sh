a580bde740eea7a43779eee4f4a8603102ffb58a
Sébastien Lucas <sebastien@slucas.fr>
2016-01-19 15:30:19 +0100

    Ajout d'une optimisation. On verra

--- test/prepareSauceTest.sh
+++ test/prepareSauceTest.sh
@@ -26,6 +26,9 @@ fi
 wget http://getcomposer.org/composer.phar
 php composer.phar install
 
+echo 'opcache.enable=1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo 'opcache.enable_cli=1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+
 # Handle Sauce
 # curl https://gist.githubusercontent.com/seblucas/7692094/raw/e2a090e6ea639a0d700e6d02cee048fa2f6c8617/sauce_connect_setup.sh | bash
 cp -v test/config_local.php.sauce config_local.php
