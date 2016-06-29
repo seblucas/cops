66962564d60a2b11a7ca82168f0dcca4dcff1a29
Markus Birth <mbirth@gmail.com>
2016-06-29 21:08:12 +0200

    Fix PHPUnit tests.

--- config.php
+++ config.php
@@ -7,9 +7,9 @@
  */
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
-require_once 'config_default.php';
+require_once dirname(__FILE__) . '/config_default.php';
 if (file_exists(dirname(__FILE__) . '/config_local.php') && (php_sapi_name() !== 'cli')) {
-    require_once 'config_local.php';
+    require_once dirname(__FILE__) . '/config_local.php';
 }
 
 $remote_user = array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['PHP_AUTH_USER'] : '';
@@ -17,7 +17,7 @@ $remote_user = array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['PHP_AUTH_
 $remote_user = preg_replace( "/[^a-zA-Z0-9_-]/", "", $remote_user);
 $user_config_file = 'config_local.' . $remote_user . '.php';
 if (file_exists(dirname(__FILE__) . '/' . $user_config_file) && (php_sapi_name() !== 'cli')) {
-    require_once $user_config_file;
+    require_once dirname(__FILE__) . '/' . $user_config_file;
 }
 
 if(!is_null($config['cops_basic_authentication']) &&
