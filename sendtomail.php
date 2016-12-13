c50e4a6e398a7834cf03d91abc89a7aa0be8e301
Sébastien Lucas <sebastien@slucas.fr>
2016-12-13 21:36:01 +0100

    Fix the mail sender.

--- sendtomail.php
+++ sendtomail.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once ("config.php");
+require_once dirname(__FILE__) . '/config.php';
+require_once dirname(__FILE__) . '/base.php';
 
 function checkConfiguration () {
     global $config;
