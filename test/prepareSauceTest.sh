65e944072af95f9f0ffe10416911e584d4335ff6
Sébastien Lucas <sebastien@slucas.fr>
2017-05-12 17:30:16 +0200

    Travis is failing to start PHP server on port 8888, try another one.

--- test/prepareSauceTest.sh
+++ test/prepareSauceTest.sh
@@ -32,7 +32,7 @@ echo 'opcache.enable_cli=1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php
 # Handle Sauce
 # curl https://gist.githubusercontent.com/seblucas/7692094/raw/e2a090e6ea639a0d700e6d02cee048fa2f6c8617/sauce_connect_setup.sh | bash
 cp -v test/config_local.php.sauce config_local.php
-php -S 127.0.0.1:8888 &
+php -S 127.0.0.1:8080 &
 vendor/bin/phpunit --no-configuration test/Sauce.php
 
 
