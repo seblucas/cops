7a1f157230380abee604a37bdfbcfb34cdad1dc1
Sébastien Lucas <sebastien@slucas.fr>
2019-06-19 21:56:01 +0200

    [fix] Moved the header before the HTML to avoid `Cannot modify header`. fix #430

--- epubreader.php
+++ epubreader.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php 
+  header('Content-Type: text/html;charset=utf-8'); 
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <?php
 /**
@@ -10,8 +12,6 @@
 
 require_once 'config.php';
 require_once 'base.php';
-
-header('Content-Type: text/html;charset=utf-8');
 
 $idData = getURLParam('data', NULL);
 $add = 'data=' . $idData . '&';
